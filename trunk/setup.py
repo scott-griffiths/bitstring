d315b9cc47d5fcfcd516a89122f102b103ae85a8
python.bitstring <python.bitstring@442ccf1e-c85e-11dd-94fd-9de6169c3690>
2009-06-03 22:27:32 +0000

    Updating version number and manifest to include new documentation.

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='bitstring',
-      version='0.4.2',
+      version='0.4.3',
       description='Simple construction, analysis and modification of binary data.',
       author='Scott Griffiths',
       author_email='scott@griffiths.name',
@@ -10,11 +10,7 @@ setup(name='bitstring',
       download_url='http://python-bitstring.googlecode.com',
       license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
       py_modules=['bitstring'],
-      long_description="""A pure Python module for analysing, constructing and modifying binary data.
-                          The underlying binary data can be interpreted as, or constructed from,
-                          hexadecimal, octal or binary strings, signed or unsigned integers,
-                          and signed or unsigned exponential-Golomb coded integers. It can
-                          also be used as and created from plain Python strings.""",
+      long_description="""A pure Python module for analysing, constructing and modifying binary data. The underlying binary data can be interpreted as, or constructed from, hexadecimal, octal or binary strings, signed or unsigned integers, and signed or unsigned exponential-Golomb coded integers. It can also be used as and created from plain Python strings.""",
       platforms='all',
       classifiers = [
         'Development Status :: 4 - Beta',
@@ -24,6 +20,8 @@ setup(name='bitstring',
         'Programming Language :: Python :: 2.4',
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
         'Topic :: Software Development :: Libraries :: Python Modules',
       ]
       )
