2d813b07451538c6a9540d04a6ed8c372683fe90
python.bitstring <python.bitstring@442ccf1e-c85e-11dd-94fd-9de6169c3690>
2009-10-10 07:44:04 +0000

    Branching from 1.0.0 release for Python 2.4 and 2.5 updates.

---
+++ branches/bitstring-1.0.0-update/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+kwds = {}
+kwds['long_description'] = open('readme.txt').read()
+
+setup(name='bitstring',
+      version='1.0.0',
+      description='Simple construction, analysis and modification of binary data.',
+      author='Scott Griffiths',
+      author_email='scott@griffiths.name',
+      url='http://python-bitstring.googlecode.com',
+      download_url='http://python-bitstring.googlecode.com',
+      license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+      py_modules=['bitstring'],
+      platforms='all',
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      **kwds
+      )
+
