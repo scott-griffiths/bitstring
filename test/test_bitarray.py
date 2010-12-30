e7bc05a76912fbb814bf8d7ffd084ae6e29129fb
python.bitstring <python.bitstring@442ccf1e-c85e-11dd-94fd-9de6169c3690>
2010-12-30 18:52:29 +0000

    Some new tests for methods which use pos, and so are failing for BitArrays.

--- test/test_bitarray.py
+++ test/test_bitarray.py
@@ -37,3 +37,55 @@ class All(unittest.TestCase):
         self.assertEqual(s.bin, '0b001010011100101110111000')
         s = BitArray('0o123')
         self.assertEqual(s.oct, '0o123')
+
+class NoPosAttribute(unittest.TestCase):
+
+    def testReplace(self):
+        s = BitArray('0b01')
+        s.replace('0b1', '0b11')
+        self.assertEqual(s, '0b011')
+
+    def testDelete(self):
+        s = BitArray('0b000000001')
+        del s[-1:]
+        self.assertEqual(s, '0b00000000')
+
+    def testInsert(self):
+        s = BitArray('0b00')
+        s.insert('0xf', 1)
+        self.assertEqual(s, '0b011110')
+
+    def testInsertParameters(self):
+        s = BitArray('0b111')
+        self.assertRaises(TypeError, s.insert, '0x4')
+
+    def testOverwrite(self):
+        s = BitArray('0b01110')
+        s.overwrite('0b000', 1)
+        self.assertEqual(s, '0b00000')
+
+    def testOverwriteParameters(self):
+        s = BitArray('0b0000')
+        self.assertRaises(TypeError, s.overwrite, '0b111')
+
+    def testPrepend(self):
+        s = BitArray('0b0')
+        s.prepend([1])
+        self.assertEqual(s, [1, 0])
+
+    def testRol(self):
+        s = BitArray('0b0001')
+        s.rol(1)
+        self.assertEqual(s, '0b0010')
+
+    def testRor(self):
+        s = BitArray('0b1000')
+        s.ror(1)
+        self.assertEqual(s, '0b0100')
+
+    def testSetItem(self):
+        s = BitArray('0b000100')
+        s[4:5] = '0xf'
+        self.assertEqual(s, '0b000111100')
+
+
