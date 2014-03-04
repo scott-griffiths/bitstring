757f23c407f029a53d481e9388c89992c741d0d7
dr.scottgriffiths@gmail.com <dr.scottgriffiths@gmail.com@442ccf1e-c85e-11dd-94fd-9de6169c3690>
2014-03-04 13:15:06 +0000

    Fixing problem with prepend when using a byteoffsetted datastore. (Issue 141). Updating version to 3.1.3

--- branches/bitstring-3.1-update/setup.py
+++ branches/bitstring-3.1-update/setup.py
@@ -14,7 +14,7 @@ if sys.version_info[:2] < (2, 6):
 # ext_modules = [Extension('bitstring', ["bitstring.pyx"], define_macros=macros)]
 
 setup(name='bitstring',
-      version='3.1.2',
+      version='3.1.3',
       description='Simple construction, analysis and modification of binary data.',
       author='Scott Griffiths',
       author_email='scott@griffiths.name',
