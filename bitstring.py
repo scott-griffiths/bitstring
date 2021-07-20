8b7ef48933b54b34a39103f4abdd0f7ee4c9df07
Scott Griffiths <dr.scottgriffiths@gmail.com>
2021-07-20 14:15:30 +0100

    Using a main() function properly and better help when using module with -m flag.

--- bitstring.py
+++ bitstring.py
@@ -4422,32 +4422,33 @@ __all__ = ['ConstBitArray', 'ConstBitStream', 'BitStream', 'BitArray',
            'Bits', 'BitString', 'pack', 'Error', 'ReadError', 'InterpretError',
            'ByteAlignError', 'CreationError', 'bytealigned', 'set_lsb0', 'set_msb0']
 
-if __name__ == '__main__':
-    """Create and interpret a bitstring from command-line parameters.
 
-    Command-line parameters are concatenated and a bitstring created
-    from them. If the final parameter is either an interpretation string
-    or ends with a '.' followed by an interpretation string then that
-    interpretation of the bitstring will be used when printing it.
-
-    Typical usage might be invoking the Python module from a console
-    as a one-off calculation:
-
-    $ python -m bitstring int:16=-400
-    0xfe70
-    $ python -m bitstring float:32=0.2 bin
-    00111110010011001100110011001101
-    $ python -m bitstring 0xff 3*0b01,0b11 uint
-    65367
-    $ python -m bitstring hex=01, uint:12=352.hex
-    01160
-
-    This feature is experimental and is subject to change or removal.
-    """
-
+def main():
     # check if final parameter is an interpretation string
     fp = sys.argv[-1]
-    if fp in name_to_read.keys():
+    if fp == '--help' or len(sys.argv) == 1:
+        print("""Create and interpret a bitstring from command-line parameters.
+
+Command-line parameters are concatenated and a bitstring created
+from them. If the final parameter is either an interpretation string
+or ends with a '.' followed by an interpretation string then that
+interpretation of the bitstring will be used when printing it.
+
+Typical usage might be invoking the Python module from a console
+as a one-off calculation:
+
+$ python -m bitstring int:16=-400
+0xfe70
+$ python -m bitstring float:32=0.2 bin
+00111110010011001100110011001101
+$ python -m bitstring 0xff 3*0b01,0b11 uint
+65367
+$ python -m bitstring hex=01, uint:12=352.hex
+01160
+
+This feature is experimental and is subject to change or removal.
+        """)
+    elif fp in name_to_read.keys():
         # concatenate all other parameters and interpret using the final one
         b1 = Bits(','.join(sys.argv[1: -1]))
         print(b1._readtoken(fp, 0, b1.__len__())[0])
@@ -4462,3 +4463,7 @@ if __name__ == '__main__':
             # No interpretation - just use default print
             b1 = Bits(','.join(sys.argv[1:]))
             print(b1)
+
+
+if __name__ == '__main__':
+    main()
