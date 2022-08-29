7b37640001abe407f4c8d8db37ec32a86099bdb2
Scott Griffiths <dr.scottgriffiths@gmail.com>
2022-08-29 20:47:23 +0100

    Trying to fix readthedocs build. Chnaged the logo colour a bit as I was bored with the whole trying to get the docs to build thing.

--- doc/conf.py
+++ doc/conf.py
@@ -92,7 +92,7 @@ html_theme_options = {
 }
 
 # The master toctree document.
-master_doc = root_doc = 'contents'
+master_doc = root_doc = 'index'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
