9471660f63a4feb1b792d70a624b05826476fbe3
Scott Griffiths <scottg@graphcore.ai>
2023-11-23 16:16:04 +0000

    Updates for Python 3.12 and bitstring 4.2.

--- doc/conf.py
+++ doc/conf.py
@@ -12,7 +12,7 @@ year = datetime.datetime.utcfromtimestamp(
 project = 'bitstring'
 copyright = f'2006 - {year}, Scott Griffiths'
 author = 'Scott Griffiths'
-release = '4.1'
+release = '4.2'
 
 extensions = []
 
