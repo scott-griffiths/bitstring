e680eaabfa70848ca7286793c07b4c500a29ba3e
Scott Griffiths <scottg@graphcore.ai>
2024-03-14 14:43:38 +0000

    Allowing MXFP formats to be created without generating the LUTs.
    This allows much nicer code to generate those LUTs by avoiding a bootstrap problem.

--- bitstring/mxfp.py
+++ bitstring/mxfp.py
@@ -6,51 +6,6 @@ from bitstring.luts import mxfp_luts_compressed
 import zlib
 from typing import Optional
 
-
-def createLUT_for_int_to_float(exp_bits: int, mantissa_bits: int, bias: int) -> array.array:
-    """Create a LUT to convert an int in representing a MXFP float into a Python float"""
-    i2f = []
-    length = 1 + exp_bits + mantissa_bits
-    for i in range(1 << length):
-        b = bitarray.util.int2ba(i, length=length, endian='big', signed=False)
-        sign = b[0]
-        exponent = bitarray.util.ba2int(b[1:1 + exp_bits])
-        significand = b[1 + exp_bits:]
-        if exponent == 0:
-            significand = bitarray.bitarray('0') + significand
-            exponent = -bias + 1
-        else:
-            significand = bitarray.bitarray('1') + significand
-            exponent -= bias
-        f = float(bitarray.util.ba2int(significand)) / (2.0 ** mantissa_bits)
-        f *= 2 ** exponent
-        if length == 8:
-            # Some special cases
-            if exp_bits == 5:
-                if i in [0b01111100, 0b11111100]:
-                    f = float('inf')
-                if i in [0b01111101, 0b11111101, 0b01111110, 0b11111110, 0b01111111, 0b11111111]:
-                    f = float('nan')
-            if exp_bits == 4:
-                if i in [0b01111111, 0b11111111]:
-                    f = float('nan')
-        i2f.append(f if not sign else -f)
-    return array.array('f', i2f)
-
-def createLUT_for_float16_to_mxfp(lut_int_to_float, exp_bits: int, mantissa_bits: int, mxfp_overflow: str) -> bytes:
-    """Create a LUT to convert a float16 into a MXFP format"""
-    # Used to create the LUT that was compressed and stored for the fp8 code
-    fp16_to_fp8 = bytearray(1 << 16)
-    for i in range(1 << 16):
-        b = struct.pack('>H', i)
-        f, = struct.unpack('>e', b)
-        fp8_i = slow_float_to_int(f, lut_int_to_float, exp_bits, mantissa_bits, mxfp_overflow)
-        if fp8_i == 1 << (exp_bits + mantissa_bits):
-            # Got back int representing binary digits for negative zero. Just convert to positive zero instead.
-            fp8_i = 0
-        fp16_to_fp8[i] = fp8_i
-    return bytes(fp16_to_fp8)
-
 def round_to_nearest_ties_to_even(lut_int_to_float, lower: int, f: float) -> Optional[int]:
     upper = lower + 1
     lower_float = lut_int_to_float[lower]
@@ -71,63 +26,6 @@ def round_to_nearest_ties_to_even(lut_int_to_float, lower: int, f: float) -> Opt
             return upper
         return lower if lower % 2 == 0 else upper
     return None
-
-def slow_float_to_int(f: float, lut_int_to_float, exp_bits: int, mantissa_bits: int, mxfp_overflow: str) -> int:
-    # Slow, but easier to follow than the faster version.
-    # The output int has the binary sequence needed for the float.
-    length = 1 + exp_bits + mantissa_bits
-    values = 1 << length
-    if f >= 0:
-        # Positive, so top bit is not set
-        for i in range(values // 2 - 1):
-            upper = lut_int_to_float[i + 1]
-            if upper == float('inf'):
-                break
-            x = round_to_nearest_ties_to_even(lut_int_to_float, i, f)
-            if x is not None:
-                return x
-        # Clip to positive max
-        if exp_bits == 5:
-            if mxfp_overflow == 'saturate':
-                return 0b01111011  # e5m2 max value of 57344
-            else:
-                assert mxfp_overflow == 'overflow'
-                return 0b01111100  # e5m2 +inf
-        if exp_bits == 4:
-            if mxfp_overflow == 'saturate':
-                return 0b01111110  # e4m3 max value of 448 (no +inf available)
-            else:
-                assert mxfp_overflow == 'overflow'
-                return 0xff  # NaN
-        return (1 << (length - 1)) - 1
-    if f < 0:
-        # Negative, so top bit is set
-        for i in range(values // 2, values - 1):
-            lower = lut_int_to_float[i + 1]
-            if lower == float('-inf'):
-                break
-            x = round_to_nearest_ties_to_even(lut_int_to_float, i, f)
-            if x is not None:
-                return x
-        # Clip to negative max
-        if exp_bits == 5:
-            if mxfp_overflow == 'saturate':
-                return 0b11111011  # e5m2 max value of -57344
-            else:
-                assert mxfp_overflow == 'overflow'
-                return 0b11111100  # e5m2 -inf
-        if exp_bits == 4:
-            if mxfp_overflow == 'saturate':
-                return 0b11111110  # e4m3 max value of -448 (no -inf available)
-            else:
-                assert mxfp_overflow == 'overflow'
-                return 0xff  # NaN
-        return (1 << length) - 1
-    if math.isnan(f):
-        if length == 8:
-            return 0xff  # Works for both e5m2 and e4m3
-        # For smaller lengths, NaN isn't supported so we instead return an invalid value
-        return 0xff
 
 
 class MXFPFormat:
@@ -157,10 +55,19 @@ class MXFPFormat:
                 self.pos_clamp_value = 0b01111100  # +inf
                 self.neg_clamp_value = 0b11111100  # -inf
 
-        int_to_float_compressed, float16_to_mxfp_compressed = mxfp_luts_compressed[(exp_bits, mantissa_bits, bias, mxfp_overflow)]
+        # If we calculate these LUTs now now it creates a bootstrap problem in generate_luts.py.
+        self.lut_float16_to_mxfp = None
+        self.lut_int_to_float = None
+
+    def decompress_luts(self):
+        int_to_float_compressed, float16_to_mxfp_compressed = mxfp_luts_compressed[(self.exp_bits, self.mantissa_bits, self.bias, self.mxfp_overflow)]
         self.lut_float16_to_mxfp = zlib.decompress(float16_to_mxfp_compressed)
         dec = zlib.decompress(int_to_float_compressed)
         self.lut_int_to_float = struct.unpack(f'<{len(dec) // 4}f', dec)
+
+    def create_luts(self):
+        self.lut_int_to_float = self.createLUT_for_int_to_float()
+        self.lut_float16_to_mxfp = self.createLUT_for_float16_to_mxfp()
 
     def float_to_int(self, f: float) -> int:
         """Given a Python float convert to the best mxfp float (expressed as an int) that represents it."""
@@ -175,6 +82,82 @@ class MXFPFormat:
         # Then use this as an index to our large LUT
         return self.lut_float16_to_mxfp[f16_int]
 
+    def slow_float_to_int(self, f: float) -> int:
+        # Slow, but easier to follow than the faster version.
+        # The output int has the binary sequence needed for the float.
+        length = 1 + self.exp_bits + self.mantissa_bits
+        values = 1 << length
+        if f >= 0:
+            # Positive, so top bit is not set
+            for i in range(values // 2 - 1):
+                upper = self.lut_int_to_float[i + 1]
+                if upper == float('inf'):
+                    break
+                x = round_to_nearest_ties_to_even(self.lut_int_to_float, i, f)
+                if x is not None:
+                    return x
+            return self.pos_clamp_value
+        if f < 0:
+            # Negative, so top bit is set
+            for i in range(values // 2, values - 1):
+                lower = self.lut_int_to_float[i + 1]
+                if lower == float('-inf'):
+                    break
+                x = round_to_nearest_ties_to_even(self.lut_int_to_float, i, f)
+                if x is not None:
+                    return x
+            # Clip to negative max
+            return self.neg_clamp_value
+        assert math.isnan(f)
+        if length == 8:
+            return 0xff  # Works for both e5m2 and e4m3
+        # For smaller lengths, NaN isn't supported so we instead return an invalid value to detect later
+        return 0xff
+
+    def createLUT_for_int_to_float(self) -> array.array:
+        """Create a LUT to convert an int in representing a MXFP float into a Python float"""
+        i2f = []
+        length = 1 + self.exp_bits + self.mantissa_bits
+        for i in range(1 << length):
+            b = bitarray.util.int2ba(i, length=length, endian='big', signed=False)
+            sign = b[0]
+            exponent = bitarray.util.ba2int(b[1:1 + self.exp_bits])
+            significand = b[1 + self.exp_bits:]
+            if exponent == 0:
+                significand = bitarray.bitarray('0') + significand
+                exponent = -self.bias + 1
+            else:
+                significand = bitarray.bitarray('1') + significand
+                exponent -= self.bias
+            f = float(bitarray.util.ba2int(significand)) / (2.0 ** self.mantissa_bits)
+            f *= 2 ** exponent
+            if length == 8:
+                # Some special cases
+                if self.exp_bits == 5:
+                    if i in [0b01111100, 0b11111100]:
+                        f = float('inf')
+                    if i in [0b01111101, 0b11111101, 0b01111110, 0b11111110, 0b01111111, 0b11111111]:
+                        f = float('nan')
+                if self.exp_bits == 4:
+                    if i in [0b01111111, 0b11111111]:
+                        f = float('nan')
+            i2f.append(f if not sign else -f)
+        return array.array('f', i2f)
+
+    def createLUT_for_float16_to_mxfp(self) -> bytes:
+        """Create a LUT to convert a float16 into a MXFP format"""
+        # Used to create the LUT that was compressed and stored for the fp8 code
+        fp16_to_fp8 = bytearray(1 << 16)
+        for i in range(1 << 16):
+            b = struct.pack('>H', i)
+            f, = struct.unpack('>e', b)
+            fp8_i = self.slow_float_to_int(f)
+            if fp8_i == 1 << (self.exp_bits + self.mantissa_bits):
+                # Got back int representing binary digits for negative zero. Just convert to positive zero instead.
+                fp8_i = 0
+            fp16_to_fp8[i] = fp8_i
+        return bytes(fp16_to_fp8)
+
 
 e2m1mxfp_fmt = MXFPFormat(exp_bits=2, mantissa_bits=1, bias=1)
 e2m3mxfp_fmt = MXFPFormat(exp_bits=2, mantissa_bits=3, bias=1)
@@ -184,3 +167,11 @@ e5m2mxfp_saturate_fmt = MXFPFormat(exp_bits=5, mantissa_bits=2, bias=15, mxfp_ov
 e4m3mxfp_overflow_fmt = MXFPFormat(exp_bits=4, mantissa_bits=3, bias=7, mxfp_overflow='overflow')
 e5m2mxfp_overflow_fmt = MXFPFormat(exp_bits=5, mantissa_bits=2, bias=15, mxfp_overflow='overflow')
 
+# These assume that the compressed LUTs have been created by generate_luts.py
+e2m1mxfp_fmt.decompress_luts()
+e2m3mxfp_fmt.decompress_luts()
+e3m2mxfp_fmt.decompress_luts()
+e4m3mxfp_saturate_fmt.decompress_luts()
+e5m2mxfp_saturate_fmt.decompress_luts()
+e4m3mxfp_overflow_fmt.decompress_luts()
+e5m2mxfp_overflow_fmt.decompress_luts()
