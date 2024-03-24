3ddf475e52d7b7af161f33818031111793dcdb69
Scott Griffiths <scottg@graphcore.ai>
2024-03-24 19:29:05 +0000

    Test commit for debugging.

--- tests/test_constbitstream.py
+++ tests/test_constbitstream.py
@@ -253,7 +253,7 @@ def test_windows_file_lock_bug():
     with open(path, mode='w') as f:
         f.write('Hello')
     # Will this lock it?
-    s = CBS(filename=path)
+    # s = CBS(filename=path)
 
     with open(path, mode='w') as f:
         f.write('Will this work?')
