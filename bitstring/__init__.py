63da8c406ffc5ac261376d1c25adece3614e3eec
Scott Griffiths <dr.scottgriffiths@gmail.com>
2026-07-17 06:38:16 +0100

    Avoiding private helpers leaking into the namespace.

--- bitstring/__init__.py
+++ bitstring/__init__.py
@@ -18,10 +18,10 @@ pack -- Create a Bits object from a format string.
 Exceptions:
 
 Error -- Module exception base class.
-CreationError -- Error during creation.
-InterpretError -- Inappropriate interpretation of binary data.
-ByteAlignError -- Whole byte position or length needed.
-ReadError -- Reading or peeking past the end of a bitstring.
+ReadError -- Reading or peeking past the end of a bitstring. Subclasses Error and IndexError.
+ByteAlignError -- Whole byte position or length needed. Subclasses Error.
+CreationError -- Error during creation. An alias for ValueError.
+InterpretError -- Inappropriate interpretation of binary data. An alias for ValueError.
 
 https://github.com/scott-griffiths/bitstring
 """
@@ -50,7 +50,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-__version__ = "5.0.0_beta1"
+__version__ = "5.0.0b1"
 
 __author__ = "Scott Griffiths"
 
@@ -63,46 +63,46 @@ from .methods import pack
 from .array_ import Array
 from .exceptions import Error, ReadError, InterpretError, ByteAlignError, CreationError
 from .dtypes import DtypeDefinition as _DtypeDefinition, dtype_register as _dtype_register, Dtype
-from typing import Literal
-from .mxfp import decompress_luts as mxfp_decompress_luts
-from .fp8 import decompress_luts as binary8_decompress_luts
+from typing import Literal as _Literal
+from .mxfp import decompress_luts as _mxfp_decompress_luts
+from .fp8 import decompress_luts as _binary8_decompress_luts
 
 # Decompress the LUTs for the exotic floating point formats
-mxfp_decompress_luts()
-binary8_decompress_luts()
+_mxfp_decompress_luts()
+_binary8_decompress_luts()
 
 # These methods convert a bit length to the number of characters needed to print it for different interpretations.
-def hex_bits2chars(bitlength: int):
+def _hex_bits2chars(bitlength: int):
     # One character for every 4 bits
     return bitlength // 4
 
 
-def oct_bits2chars(bitlength: int):
+def _oct_bits2chars(bitlength: int):
     # One character for every 3 bits
     return bitlength // 3
 
 
-def bin_bits2chars(bitlength: int):
+def _bin_bits2chars(bitlength: int):
     # One character for each bit
     return bitlength
 
 
-def bytes_bits2chars(bitlength: int):
+def _bytes_bits2chars(bitlength: int):
     # One character for every 8 bits
     return bitlength // 8
 
 
-def uint_bits2chars(bitlength: int):
+def _uint_bits2chars(bitlength: int):
     # How many characters is largest possible int of this length?
     return len(str((1 << bitlength) - 1))
 
 
-def int_bits2chars(bitlength: int):
+def _int_bits2chars(bitlength: int):
     # How many characters is largest negative int of this length? (To include minus sign).
     return len(str(-1 << (bitlength - 1)))
 
 
-def float_bits2chars(bitlength: Literal[16, 32, 64]):
+def _float_bits2chars(bitlength: _Literal[16, 32, 64]):
     # These bit lengths were found by looking at lots of possible values
     if bitlength in [16, 32]:
         return 23  # Empirical value
@@ -110,113 +110,113 @@ def float_bits2chars(bitlength: Literal[16, 32, 64]):
         return 24  # Empirical value
 
 
-def p3binary_bits2chars(_: Literal[8]):
+def _p3binary_bits2chars(_: _Literal[8]):
     return 19  # Empirical value
 
 
-def p4binary_bits2chars(_: Literal[8]):
+def _p4binary_bits2chars(_: _Literal[8]):
     # Found by looking at all the possible values
     return 13  # Empirical value
 
 
-def e4m3mxfp_bits2chars(_: Literal[8]):
+def _e4m3mxfp_bits2chars(_: _Literal[8]):
     return 13
 
 
-def e5m2mxfp_bits2chars(_: Literal[8]):
+def _e5m2mxfp_bits2chars(_: _Literal[8]):
     return 19
 
 
-def e3m2mxfp_bits2chars(_: Literal[6]):
+def _e3m2mxfp_bits2chars(_: _Literal[6]):
     # Not sure what the best value is here. It's 7 without considering the scale that could be applied.
     return 7
 
 
-def e2m3mxfp_bits2chars(_: Literal[6]):
+def _e2m3mxfp_bits2chars(_: _Literal[6]):
     # Not sure what the best value is here.
     return 7
 
 
-def e2m1mxfp_bits2chars(_: Literal[4]):
+def _e2m1mxfp_bits2chars(_: _Literal[4]):
     # Not sure what the best value is here.
     return 7
 
 
-def e8m0mxfp_bits2chars(_: Literal[8]):
+def _e8m0mxfp_bits2chars(_: _Literal[8]):
     # Has same range as float32
     return 23
 
 
-def mxint_bits2chars(_: Literal[8]):
+def _mxint_bits2chars(_: _Literal[8]):
     # Not sure what the best value is here.
     return 10
 
 
-def bfloat_bits2chars(_: Literal[16]):
+def _bfloat_bits2chars(_: _Literal[16]):
     # Found by looking at all the possible values
     return 23  # Empirical value
 
 
-def bits_bits2chars(bitlength: int):
+def _bits_bits2chars(bitlength: int):
     # For bits type we can see how long it needs to be printed by trying any value
     temp = Bits.from_zeros(bitlength)
     return len(str(temp))
 
 
-def bool_bits2chars(_: Literal[1]):
+def _bool_bits2chars(_: _Literal[1]):
     # Bools are printed as 1 or 0, not True or False, so are one character each
     return 1
 
-dtype_definitions = [
+_dtype_definitions = [
     # Integer types
-    _DtypeDefinition('u', Bits._setuint, Bits._getuint, int, False, uint_bits2chars,
+    _DtypeDefinition('u', Bits._setuint, Bits._getuint, int, False, _uint_bits2chars,
                     read_fn=Bits._readuint,
                     description="a two's complement unsigned int"),
-    _DtypeDefinition('ule', Bits._setuintle, Bits._getuintle, int, False, uint_bits2chars,
+    _DtypeDefinition('ule', Bits._setuintle, Bits._getuintle, int, False, _uint_bits2chars,
                     read_fn=Bits._readuintle,
                     allowed_lengths=(8, 16, 24, ...), description="a two's complement little-endian unsigned int"),
-    _DtypeDefinition('ube', Bits._setuintbe, Bits._getuintbe, int, False, uint_bits2chars,
+    _DtypeDefinition('ube', Bits._setuintbe, Bits._getuintbe, int, False, _uint_bits2chars,
                     read_fn=Bits._readuintbe,
                     allowed_lengths=(8, 16, 24, ...), description="a two's complement big-endian unsigned int"),
-    _DtypeDefinition('i', Bits._setint, Bits._getint, int, True, int_bits2chars,
+    _DtypeDefinition('i', Bits._setint, Bits._getint, int, True, _int_bits2chars,
                     read_fn=Bits._readint,
                     description="a two's complement signed int"),
-    _DtypeDefinition('ile', Bits._setintle, Bits._getintle, int, True, int_bits2chars,
+    _DtypeDefinition('ile', Bits._setintle, Bits._getintle, int, True, _int_bits2chars,
                     read_fn=Bits._readintle,
                     allowed_lengths=(8, 16, 24, ...), description="a two's complement little-endian signed int"),
-    _DtypeDefinition('ibe', Bits._setintbe, Bits._getintbe, int, True, int_bits2chars,
+    _DtypeDefinition('ibe', Bits._setintbe, Bits._getintbe, int, True, _int_bits2chars,
                     read_fn=Bits._readintbe,
                     allowed_lengths=(8, 16, 24, ...), description="a two's complement big-endian signed int"),
     # String types
-    _DtypeDefinition('hex', Bits._sethex, Bits._gethex, str, False, hex_bits2chars,
+    _DtypeDefinition('hex', Bits._sethex, Bits._gethex, str, False, _hex_bits2chars,
                     read_fn=Bits._readhex,
                     allowed_lengths=(0, 4, 8, ...), description="a hexadecimal string"),
-    _DtypeDefinition('bin', Bits._setbin, Bits._getbin, str, False, bin_bits2chars,
+    _DtypeDefinition('bin', Bits._setbin, Bits._getbin, str, False, _bin_bits2chars,
                     read_fn=Bits._readbin,
                     description="a binary string"),
-    _DtypeDefinition('oct', Bits._setoct, Bits._getoct, str, False, oct_bits2chars,
+    _DtypeDefinition('oct', Bits._setoct, Bits._getoct, str, False, _oct_bits2chars,
                     read_fn=Bits._readoct,
                     allowed_lengths=(0, 3, 6, ...), description="an octal string"),
     # Float types
-    _DtypeDefinition('f', Bits._setfloatbe, Bits._getfloatbe, float, True, float_bits2chars,
+    _DtypeDefinition('f', Bits._setfloatbe, Bits._getfloatbe, float, True, _float_bits2chars,
                     read_fn=Bits._readfloatbe,
                     allowed_lengths=(16, 32, 64), description="a big-endian floating point number"),
-    _DtypeDefinition('fle', Bits._setfloatle, Bits._getfloatle, float, True, float_bits2chars,
+    _DtypeDefinition('fle', Bits._setfloatle, Bits._getfloatle, float, True, _float_bits2chars,
                     read_fn=Bits._readfloatle,
                     allowed_lengths=(16, 32, 64), description="a little-endian floating point number"),
-    _DtypeDefinition('bfloat', Bits._setbfloatbe, Bits._getbfloatbe, float, True, bfloat_bits2chars,
+    _DtypeDefinition('bfloat', Bits._setbfloatbe, Bits._getbfloatbe, float, True, _bfloat_bits2chars,
                     read_fn=Bits._readbfloatbe,
                     allowed_lengths=(16,), description="a 16 bit big-endian bfloat floating point number"),
-    _DtypeDefinition('bfloatle', Bits._setbfloatle, Bits._getbfloatle, float, True, bfloat_bits2chars,
+    _DtypeDefinition('bfloatle', Bits._setbfloatle, Bits._getbfloatle, float, True, _bfloat_bits2chars,
                     read_fn=Bits._readbfloatle,
                     allowed_lengths=(16,), description="a 16 bit little-endian bfloat floating point number"),
     # Other known length types
-    _DtypeDefinition('bits', Bits._setbits, Bits._getbits, Bits, False, bits_bits2chars,
+    _DtypeDefinition('bits', Bits._setbits, Bits._getbits, Bits, False, _bits_bits2chars,
                     description="a bitstring object"),
-    _DtypeDefinition('bool', Bits._setbool, Bits._getbool, bool, False, bool_bits2chars,
+    _DtypeDefinition('bool', Bits._setbool, Bits._getbool, bool, False, _bool_bits2chars,
                     read_fn=Bits._readbool,
                     allowed_lengths=(1,), description="a bool (True or False)"),
-    _DtypeDefinition('bytes', Bits._setbytes, Bits._getbytes, bytes, False, bytes_bits2chars,
+    _DtypeDefinition('bytes', Bits._setbytes, Bits._getbytes, bytes, False, _bytes_bits2chars,
                     read_fn=Bits._readbytes,
                     multiplier=8, description="a bytes object"),
     # Unknown length types
@@ -234,43 +234,43 @@ dtype_definitions = [
                     description="a skipped section of padding"),
 
     # MXFP and IEEE 8-bit float types
-    _DtypeDefinition('p3binary', Bits._setp3binary, Bits._getp3binary, float, True, p3binary_bits2chars,
+    _DtypeDefinition('p3binary', Bits._setp3binary, Bits._getp3binary, float, True, _p3binary_bits2chars,
                     read_fn=Bits._readp3binary,
                     allowed_lengths=(8,), description="an 8 bit float with binary8p3 format"),
-    _DtypeDefinition('p4binary', Bits._setp4binary, Bits._getp4binary, float, True, p4binary_bits2chars,
+    _DtypeDefinition('p4binary', Bits._setp4binary, Bits._getp4binary, float, True, _p4binary_bits2chars,
                     read_fn=Bits._readp4binary,
                     allowed_lengths=(8,), description="an 8 bit float with binary8p4 format"),
-    _DtypeDefinition('e4m3mxfp_saturate', Bits._sete4m3mxfp_saturate, Bits._gete4m3mxfp, float, True, e4m3mxfp_bits2chars,
+    _DtypeDefinition('e4m3mxfp_saturate', Bits._sete4m3mxfp_saturate, Bits._gete4m3mxfp, float, True, _e4m3mxfp_bits2chars,
                     read_fn=Bits._reade4m3mxfp,
                     allowed_lengths=(8,), description="an 8 bit float with MXFP E4M3 format, saturating out-of-range values"),
-    _DtypeDefinition('e4m3mxfp_overflow', Bits._sete4m3mxfp_overflow, Bits._gete4m3mxfp, float, True, e4m3mxfp_bits2chars,
+    _DtypeDefinition('e4m3mxfp_overflow', Bits._sete4m3mxfp_overflow, Bits._gete4m3mxfp, float, True, _e4m3mxfp_bits2chars,
                     read_fn=Bits._reade4m3mxfp,
                     allowed_lengths=(8,), description="an 8 bit float with MXFP E4M3 format, overflowing out-of-range values"),
-    _DtypeDefinition('e5m2mxfp_saturate', Bits._sete5m2mxfp_saturate, Bits._gete5m2mxfp, float, True, e5m2mxfp_bits2chars,
+    _DtypeDefinition('e5m2mxfp_saturate', Bits._sete5m2mxfp_saturate, Bits._gete5m2mxfp, float, True, _e5m2mxfp_bits2chars,
                     read_fn=Bits._reade5m2mxfp,
                     allowed_lengths=(8,), description="an 8 bit float with MXFP E5M2 format, saturating out-of-range values"),
-    _DtypeDefinition('e5m2mxfp_overflow', Bits._sete5m2mxfp_overflow, Bits._gete5m2mxfp, float, True, e5m2mxfp_bits2chars,
+    _DtypeDefinition('e5m2mxfp_overflow', Bits._sete5m2mxfp_overflow, Bits._gete5m2mxfp, float, True, _e5m2mxfp_bits2chars,
                     read_fn=Bits._reade5m2mxfp,
                     allowed_lengths=(8,), description="an 8 bit float with MXFP E5M2 format, overflowing out-of-range values"),
-    _DtypeDefinition('e3m2mxfp', Bits._sete3m2mxfp, Bits._gete3m2mxfp, float, True, e3m2mxfp_bits2chars,
+    _DtypeDefinition('e3m2mxfp', Bits._sete3m2mxfp, Bits._gete3m2mxfp, float, True, _e3m2mxfp_bits2chars,
                     read_fn=Bits._reade3m2mxfp,
                     allowed_lengths=(6,), description="a 6 bit float with MXFP E3M2 format"),
-    _DtypeDefinition('e2m3mxfp', Bits._sete2m3mxfp, Bits._gete2m3mxfp, float, True, e2m3mxfp_bits2chars,
+    _DtypeDefinition('e2m3mxfp', Bits._sete2m3mxfp, Bits._gete2m3mxfp, float, True, _e2m3mxfp_bits2chars,
                     read_fn=Bits._reade2m3mxfp,
                     allowed_lengths=(6,), description="a 6 bit float with MXFP E2M3 format"),
-    _DtypeDefinition('e2m1mxfp', Bits._sete2m1mxfp, Bits._gete2m1mxfp, float, True, e2m1mxfp_bits2chars,
+    _DtypeDefinition('e2m1mxfp', Bits._sete2m1mxfp, Bits._gete2m1mxfp, float, True, _e2m1mxfp_bits2chars,
                     read_fn=Bits._reade2m1mxfp,
                     allowed_lengths=(4,), description="a 4 bit float with MXFP E2M1 format"),
-    _DtypeDefinition('e8m0mxfp', Bits._sete8m0mxfp, Bits._gete8m0mxfp, float, False, e8m0mxfp_bits2chars,
+    _DtypeDefinition('e8m0mxfp', Bits._sete8m0mxfp, Bits._gete8m0mxfp, float, False, _e8m0mxfp_bits2chars,
                     read_fn=Bits._reade8m0mxfp,
                     allowed_lengths=(8,), description="an 8 bit float with MXFP E8M0 format"),
-    _DtypeDefinition('mxint', Bits._setmxint, Bits._getmxint, float, True, mxint_bits2chars,
+    _DtypeDefinition('mxint', Bits._setmxint, Bits._getmxint, float, True, _mxint_bits2chars,
                     read_fn=Bits._readmxint,
                     allowed_lengths=(8,), description="an 8 bit float with MXFP INT8 format"),
 ]
 
 
-aliases: list[tuple[str, str]] = [
+_aliases: list[tuple[str, str]] = [
     # Floats default to big endian
     ('f', 'float'),
     ('f', 'floatbe'),
@@ -290,19 +290,20 @@ aliases: list[tuple[str, str]] = [
 ]
 
 
-for dt in dtype_definitions:
-    _dtype_register.add_dtype(dt)
-for alias in aliases:
-    _dtype_register.add_dtype_alias(alias[0], alias[1])
+for _dt in _dtype_definitions:
+    _dtype_register.add_dtype(_dt)
+for _alias in _aliases:
+    _dtype_register.add_dtype_alias(_alias[0], _alias[1])
+del _dt, _alias
 
-property_docstrings = [f'{name} -- Interpret as {_dtype_register[name].description}.' for name in _dtype_register.names]
-property_docstring = '\n    '.join(property_docstrings)
+_property_docstrings = [f'{name} -- Interpret as {_dtype_register[name].description}.' for name in _dtype_register.names]
+_property_docstring = '\n    '.join(_property_docstrings)
 
 # We can't be sure the docstrings are present, as it might be compiled without docstrings.
 if Bits.__doc__ is not None:
-    Bits.__doc__ = Bits.__doc__.replace('[GENERATED_PROPERTY_DESCRIPTIONS]', property_docstring)
+    Bits.__doc__ = Bits.__doc__.replace('[GENERATED_PROPERTY_DESCRIPTIONS]', _property_docstring)
 if BitArray.__doc__ is not None:
-    BitArray.__doc__ = BitArray.__doc__.replace('[GENERATED_PROPERTY_DESCRIPTIONS]', property_docstring)
+    BitArray.__doc__ = BitArray.__doc__.replace('[GENERATED_PROPERTY_DESCRIPTIONS]', _property_docstring)
 __all__ = ['Reader', 'BitArray', 'Array',
            'Bits', 'pack', 'Error', 'ReadError', 'InterpretError',
            'ByteAlignError', 'CreationError', 'Dtype']
